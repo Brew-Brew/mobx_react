ef1eaae520d80903ad018d744a5fbcd490a5d76e
seungkyu2 <zx6658@naver.com>
2018-08-11 16:53:09 +0900

    apply mobx

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ learn mobx with react
 ```
 
 1. cra는 @observer를 지원안함 -> syntax error
+- 해결방법: 
+https://stackoverflow.com/questions/41138534/mobx-react-unexpected-token 참고 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TodoList from './TodoList'
 import registerServiceWorker from './registerServiceWorker';
 
 
-const store = new App();
+const store = new TodoList();
 ReactDOM.render(<App todoList={store} />, document.getElementById('root'));
 registerServiceWorker();
